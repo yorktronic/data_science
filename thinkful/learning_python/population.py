b684fe3b0019beebfb32166130f61b450657434f
yorktronic <ty.walker@gmail.com>
2015-05-23 11:36:25 -0700

    completed population increase exercise

---
+++ thinkful/learning_python/population.py
@@ -0,0 +1,34 @@
+import collections
+population_dict = collections.defaultdict(int)
+land_area = collections.defaultdict(int)
+population_density = collections.defaultdict(float)
+
+# Open the population data file with Unversal read
+inputFile = open('./population_data/lecz-urban-rural-population-land-area-estimates_continent-90m.csv','rU')
+
+# Store and skip the header
+header = next(inputFile)
+
+# Add entries to populationdict for each continent, and sum the urban and rural populations of each continent
+for line in inputFile:
+	line = line.rstrip().split(',')
+	line[5] = int(line[5])
+	if line[1] == 'Total National Population':
+ 		population_dict[line[0]] += line[5]
+ 		land_area[line[0]] = int(line[7])
+
+
+	#Calculates the population density
+for k, v in population_dict.iteritems():
+	population_density[k] = v / land_area[k]
+
+
+# From the previous version where I wrote the populations to a file
+# with open('national_population.csv', 'w') as outputFile:
+    #outputFile.write('continent,2010_population\n')
+
+    #for k, v in population_dict.iteritems():
+     #   outputFile.write(k + ',' + str(v) + '\n')
+
+
+
---
+++ thinkful/learning_python/population_increase_2010_2100.py
@@ -0,0 +1,49 @@
+# Determine the population increase in the low elevation zones of each country from the year 2010 to the year 2100
+
+# Input = population data file
+# Output = dict with the country and the percentage population increase from year 2010 to 2100
+
+import collections
+population_2010 = collections.defaultdict(int)
+population_2100 = collections.defaultdict(int)
+
+# This dictionary will contain a 2-entry list for each country. The first entry will be % change in population, the second entry will be the total difference in population
+population_diff = collections.defaultdict(int)
+population_percentage_diff = collections.defaultdict(float)
+
+# Open the population data file 
+inputFile = open('./population_data/lecz-urban-rural-population-land-area-estimates_country-90m.csv','rU')
+
+# Store and skip the header
+header = next(inputFile)
+
+# Add each countries 2010 population to population_2010, and add each contries 2100 population to populatio_2100
+
+for line in inputFile:
+	line = line.rstrip().split(',')
+	
+	line[11] = int(line[11])
+	line[12] = int(line[12])
+
+	population_2010[line[2]] += line[11]
+	population_2100[line[2]] += line[12]
+
+# Populate percentages
+for k, v in population_2010.iteritems():
+	population_diff[k] = population_2100[k] - v
+
+	if population_2100[k] > population_2010[k]:
+		population_percentage_diff[k] = "%.2f" % ((((float(population_2100[k]) / float(v)) - 1) * 100))
+	elif population_2100[k] < population_2010[k]:
+		population_percentage_diff[k] = "%.2f" % (-(1 - ((float(population_2100[k]) / float(v))))*100)
+	else:
+		population_percentage_diff[k] = 0.0
+
+
+
+
+
+
+
+
+
