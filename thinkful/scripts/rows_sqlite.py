04343574f6183b1cdc18cf7eda72348b4246b89a
yorktronic <ty.walker@gmail.com>
2015-05-27 20:26:42 -0700

    Fixed error in rows_sqlite.

--- thinkful/scripts/rows_sqlite.py
+++ thinkful/scripts/rows_sqlite.py
@@ -8,13 +8,13 @@ con = lite.connect('../db/getting_started.db')
 
 # Inserting rows to db
 cur = con.cursor()
-cur.execute("INSERT INTO cities VALUES('Washington', 'DC')")
-cur.execute("INSERT INTO cities VALUES('Houston', 'TX')")
-cur.execute("INSERT INTO weather VALUES('Washington', 2013, 'July', 'January', 65)")
-cur.execute("INSERT INTO weather VALUES('Houston', 2013, 'July', 'January', 79)")
 
-# A more efficient way is to use executemany()
-cur.executemany("INSERT INTO cities VALUES(?,?)", cities)
-cur.executemany("INSERT INTO weather VALUES(?,?,?,?,?)", weather)
+with con:
+	cur.execute("INSERT INTO cities VALUES('Washington', 'DC')")
+	cur.execute("INSERT INTO cities VALUES('Houston', 'TX')")
+	cur.execute("INSERT INTO weather VALUES('Washington', 2013, 'July', 'January', 65)")
+	cur.execute("INSERT INTO weather VALUES('Houston', 2013, 'July', 'January', 79)")
 
-
+	# A more efficient way is to use executemany()
+	cur.executemany("INSERT INTO cities VALUES(?,?)", cities)
+	cur.executemany("INSERT INTO weather VALUES(?,?,?,?,?)", weather)
