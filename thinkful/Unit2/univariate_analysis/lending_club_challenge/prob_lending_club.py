bc6efff0bc0032f7f97e4df807912d684f72a358
yorktronic <ty.walker@gmail.com>
2015-06-05 11:11:37 -0700

    lending_club.py and prob_lending_club.py working properly-ish, and uploaded plots

--- thinkful/Unit2/univariate_analysis/lending_club_challenge/prob_lending_club.py
+++ thinkful/Unit2/univariate_analysis/lending_club_challenge/prob_lending_club.py
@@ -10,11 +10,18 @@ loansData.dropna(inplace = True)
 
 data = loansData[['Amount.Requested', 'Amount.Funded.By.Investors']]
 
+# Rather than just plotting the Amount Requested by itself, I created a boxplot and a histogram with both Amount Requested and Amount Funded by Investors on the same plot
+plt.figure()
 data.boxplot()
 plt.savefig('./plots/boxplot_amt_requested_v_funded.png')
 plt.clf()
+
 
 data.hist()
 plt.savefig('./plots/histogram_amt_requested_v_funded.png')
 plt.clf()
 
+# I don't think the QQ plot can handle two plots on the same figure, so I'm plotting them independently
+stats.probplot(loansData['Amount.Funded.By.Investors'], dist = "norm", plot = plt)
+plt.savefig('./plots/amt_funded_by_investors_v_normal_distribution.png')
+plt.clf()
