8192f2e89d9b25dccff6afa397195c41572a631a
yorktronic <ty.walker@gmail.com>
2015-06-15 17:23:02 -0700

    added scatter plot functionality to linear_regression.py

--- thinkful/Unit2/linear_regression_and_correlation/linear_regression.py
+++ thinkful/Unit2/linear_regression_and_correlation/linear_regression.py
@@ -14,19 +14,22 @@ loansData.dropna(inplace=True)
 #
 cleanRate = loansData['Interest.Rate'].map(lambda x: round((float(x.rstrip('%')) / 100), 4))
 
+# Create a new set of loan lengths with the word "months" removed
+# 
+noMonths = loansData['Loan.Length'].map(lambda x: x.rstrip(' months'))
+
 #...FICO scores by picking the first value out of the range
 #
 cleanFICO = loansData['FICO.Range'].map(lambda x: int(x.split('-')[0]))
 
-# add these two sets as new columns in the data set (NOTE: this way only works if you know that the index values of the DataFrame are continuous. If you are dealing with non-continuous index values, see http://stackoverflow.com/questions/12555323/adding-new-column-to-existing-dataframe-in-python-pandas for the proper solution)
+# replace Interest.Rate and Loan.Length with the cleaned versions, and add FICO.Score as a new column (NOTE: this way only works if you know that the index values of the DataFrame are continuous. If you are dealing with non-continuous index values, see http://stackoverflow.com/questions/12555323/adding-new-column-to-existing-dataframe-in-python-pandas for the proper solution)
 #
-
 loansData['FICO.Score'] = cleanFICO
-loansData['Clean.Interest.Rate'] = cleanRate
+loansData['Interest.Rate'] = cleanRate
+loansData['Loan.Length'] = noMonths
 
 # Create the histogram
 #
-#plt.figure()
 loansData.hist(column='FICO.Score')
 
 # Tweak the spacing to prevent clipping of tick-labels
@@ -40,5 +43,6 @@ plt.clf()
 
 # Note - if we had wanted to rotate the x-axis labels, we could've used: plt.xticks(rotation='vertical')
 
+# Create a scatterplot of the data to locate any trends / interrelationships between column data
 scat = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
 plt.show()
