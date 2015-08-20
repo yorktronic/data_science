1dd0ea8b79dcc1f6a003c2aa90c7a0ba73bfa900
yorktronic <ty.walker@gmail.com>
2015-08-19 17:09:02 -0700

    cleaning up code / comments

--- thinkful/Unit3/weather/analysis.py
+++ thinkful/Unit3/weather/analysis.py
@@ -16,7 +16,13 @@ con = lite.connect('./db/weather.db')
 maxTemps = pd.read_sql_query("SELECT * FROM daily_max_temperature", con)
 
 # List of cities, to make things easy
-cities = ['atlanta', 'austin', 'boston', 'chicago', 'cleveland', 'denver', 'lasVegas', 'miami', 'minneapolis', 'nashville', 'newOrleans', 'newYork', 'philadelphia', 'phoenix', 'saltLakeCity', 'sanFrancisco', 'seattle', 'washington']
+cities = ['atlanta', 'austin', 'boston', 'chicago', 'cleveland', 'denver', 'lasVegas', 'miami',
+			'minneapolis', 'nashville', 'newOrleans', 'newYork', 'philadelphia', 'phoenix', 
+			'saltLakeCity', 'sanFrancisco', 'seattle', 'washington']
+
+########################
+# Basic stats analysis #
+########################
 
 # Calculate the range, mean, median, and variance of each city in the list
 ranges = {}
@@ -51,22 +57,8 @@ swing_max = (swing_max_city, swing_max_value)
 var_max_city = max(variances, key=variances.get) 
 var_max_value = variances[var_max_city]
 var_max = (var_max_city, var_max_value)
-# Answer: Seattle
 
-# Plot the distribution of the difference from the mean for Seattle
-# Create a list of all the differences
-seattle_diff = []
-for temp in maxTemps['seattle']:
-	seattle_diff.append(means['seattle'] - temp)
-
-seattle_diff_series = pd.Series(seattle_diff) # Convert the list in to a Series so that you can plot it
-
-plt.figure()
-qq_norm = stats.probplot(seattle_diff_series, dist='norm', plot=plt)
-plt.savefig('./plots/seattle_difference_plot.png')
-plt.clf()
-
-# What was the highest temperature overall and where was it?
+# Calculate the highest temperature overall and where it was
 temp_max_value = 0
 temp_max_city = ''
 for city in ranges:
@@ -75,7 +67,28 @@ for city in ranges:
 		temp_max_city = city
 temp_max = (temp_max_city, temp_max_value)
 
-# Plot a histogram of every city with temps rounded to the nearest degree F
+#####################################################################
+# Plot the distribution of the difference from the mean for Seattle #
+#####################################################################
+
+# Create a list of all the differences
+seattle_diff = []
+for temp in maxTemps['seattle']:
+	seattle_diff.append(means['seattle'] - temp)
+
+# Convert the list in to a Series so that you can plot it
+seattle_diff_series = pd.Series(seattle_diff) 
+
+# Plot and save the figure
+plt.figure()
+qq_norm = stats.probplot(seattle_diff_series, dist='norm', plot=plt)
+plt.savefig('./plots/seattle_difference_plot.png')
+plt.clf()
+
+######################################################################################
+# Plot a histogram of every city with temps rounded to the nearest degree Fahrenheit #
+######################################################################################
+
 # Convert the objects to numeric (otherwise we can't round them)
 df = maxTemps.convert_objects(convert_numeric = True)
 df = np.round(df, 0)
@@ -95,4 +108,5 @@ for city in cities:
 	plt.savefig('./plots/qq_plots/{}_qq.png'.format(city))
 	plt.clf()
 
+# Close all plots
 plt.close()
